5094671eb2b9a89f8be3e9c21f29cae043f09ac1
Leif Lundgren <l@l>
2019-03-31 14:10:27 +0200

    main file

---
+++ __main__.py
@@ -0,0 +1,12 @@
+import sys
+
+from command_line_parser import CommandLineParser
+from settings import Settings
+from tools import trace
+
+def main(argv:[str]):
+    trace(1, "placeholder for main method")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
